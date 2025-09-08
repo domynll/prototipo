dc06aeeea12d2fc42bf8f4a9b9f43ab9ccc6bf95
merlypaola <paolamerly1@gmail.com>
2025-09-08 14:38:18 -0500

    actualizacion

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,19 @@
-// tailwind.config.cjs
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#047857",   // --primary
+          dark: "#064e3b",      // --primary-dark
+          light: "#10b981",     // --primary-light
+        },
+        accent: "#3b82f6",
+      }
+    },
   },
   plugins: [],
 }
